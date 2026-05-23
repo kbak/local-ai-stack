cd1123e2a20908749002513b91eff60eafccf3a6
kbak <290936+kbak@users.noreply.github.com>
2026-05-23 16:37:53 -0700

    Removed hsts

--- nextcloud/hooks/before-starting/hsts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cat > /etc/apache2/conf-available/hsts.conf << 'CONF'
-<IfModule mod_headers.c>
-    Header always set Strict-Transport-Security "max-age=15552000; includeSubDomains"
-</IfModule>
-CONF
-a2enconf hsts >/dev/null 2>&1
